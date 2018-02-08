5bcb88ab1b07970fabab175d854aa57756071707
Clark.Tiernan <Clark.Tiernan@SEMS-259-09.SEMS259.LAB>
2018-02-07 16:24:25 -0800

    crazy cat lady

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,18 +5,28 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
-		// 1. Ask the user how many cats they have
-
-		// 2. Convert their answer into an int
-
-		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
-
-		// 4. If they have 3 or less, call the method below to show them a cat video
-
-		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
 		
+		// 1. Ask the user how many cats they have
+String  s  = JOptionPane.showInputDialog(null, "How many cats do you have");
+		// 2. Convert their answer into an dint
+int cats = Integer.parseInt(s);
+		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
+if(cats>3&&cats<0) {
+	
+}
+JOptionPane.showMessageDialog(null, "YOU ARE A CRAZY CAT LADY!!!!!! DO YOU KNOW HOW MUCH POOP YOU HAVE TO PICK UP??????????????!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		// 4. If they have 3 or less, call the method below to show them a cat video
+if (cats<3&&cats>0) {
+	
+
+ playVideo("https://www.youtube.com/watch?v=iRXJXaLV0n4");
+}
+		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
+playVideo("https://www.youtube.com/watch?v=JuDTJ0iSk3U");
 	}
 
 	static void playVideo(String videoURL) {
@@ -26,7 +36,6 @@ public class CrazyCatLady {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
-
 }
-
