1c2e227088a27036396b5654efa5a5ef48b629b3
Clark.Tiernan <Clark.Tiernan@SEMS-259-07.SEMS259.LAB>
2018-05-09 16:28:06 -0700

    # WARNING: head commit changed in the meantime
    ValentineRobotDrawingAHeart

--- src/elseif/ValentineRobotDrawingAHeart.java
+++ src/elseif/ValentineRobotDrawingAHeart.java
@@ -3,29 +3,27 @@ package elseif;
 import org.jointheleague.graphical.robot.Robot;
 
 public class ValentineRobotDrawingAHeart {
-public static void main(String[] args) {
-	
-
-	Robot rocky = new Robot();
-rocky.setPenColor(244,66,98);	
-rocky.penDown();
-rocky.setSpeed(2000);
-rocky.setAngle(30);
-rocky.move(100);
-for (int i = 0; i < 180; i++) {
-rocky.turn(-360/360);	
-rocky.move(2);}
-rocky.setAngle(0);
-for (int j = 0; j < 180; j++) {
-rocky.turn(-360/360);
-rocky.move(2);}
+	public static void main(String[] args) {
 
 
+		Robot rocky = new Robot();
+		rocky.setPenColor(244,66,98);	
+		rocky.penDown();
+		rocky.setSpeed(8000);
+		rocky.setAngle(30);
+		rocky.move(100);
+		for (int i = 0; i < 180; i++) {
+			rocky.turn(-360/360);	
+			rocky.move(2);}
+		rocky.setAngle(0);
+		for (int j = 0; j < 180; j++) {
+			rocky.turn(-360/360);
+			rocky.move(2);}
+		    rocky.turn(-10);
+			rocky.move(470);
+			rocky.turn(-120);
+			rocky.move(400);
+		
 
-	
+	}
 }
- 
-	
-}
-
-
