a6d1380a5d9f541640b8f0c4c7d371170823cb9b
Clark.Tiernan <Clark.Tiernan@SEMS-259-07.SEMS259.LAB>
2018-05-16 16:30:03 -0700

    org.eclipse bananaQuiz fizzBuzz

--- src/extras/BananaQuiz.java
+++ src/extras/BananaQuiz.java
@@ -4,22 +4,25 @@ package extras;
 
 import javax.swing.JOptionPane;
 
-public class BananaQuiz
-{
-	
-	public static void main(String[] args)
-	{
-		//1. ask the user if they like bananas
-		//2. if they say no, 
-			//tell them they are crazy 
-			//and end quiz
-		//3. if they say yes
-		//	ask them what is their favorite hobby
-		//	show a pop up that says "<your hobby> is much better with bananas!"
+public class BananaQuiz {
 
-		//4. OPTIONAL: if they say something other than “yes” or “no”
-		//	show a pop up that says “You are bananas!”
-	
+	public static void main(String[] args) {
+		// 1. ask the user if they like bananas
+		String answer = JOptionPane.showInputDialog(null, " ................. Do you like bananas...............");
+		// 2. if they say no,
+		// tell them they are crazy
+		// and end the quiz
+		if (answer.equals("no")) {
+			JOptionPane.showMessageDialog(null, " You are a CRAZY! BANANAS!");
+		}
+		// 3. if they say yes
+		// ask them what is their favorite hobby
+		// show a pop up that says "<your hobby> is much better with bananas!"
+if (answer.equals("yes")) {
+JOptionPane.showMessageDialog(null, " Music is much better with banana drumsticks, don't you think?");
+}
+		// 4. OPTIONAL: if they say something other than yes or no
+		// show a pop up that says that you are bananas!â€�
+
 	}
-
 }
